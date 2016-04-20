4eb8a0ec27b74e190cf695e74d8ebc8c736176c6
Varun <varunv@hifx.co.in>
2016-04-20 14:37:56 +0530

    errortypes redefined

--- errortypes.go
+++ errortypes.go
@@ -19,148 +19,160 @@ func wrap(err error, code int, format, suffix string, args ...interface{}) Err {
 	return newErr
 }
 
-// internalServer represents an error when something unexpected has happened.
-type internalServer struct {
-	Err
-}
+// InternalServer represents an error when something unexpected has happened.
 
 // InternalServerf returns an error which satisfies IsInternalServer().
 func InternalServerf(format string, args ...interface{}) error {
-	return &internalServer{wrap(nil, http.StatusInternalServerError, format, "", args...)}
+	e := wrap(nil, http.StatusInternalServerError, format, "", args...)
+	return &e
 }
 
 // NewInternalServer returns an error which wraps err that satisfies
 // IsInternalServer().
 func NewInternalServer(err error, msg string) error {
-	return &internalServer{wrap(err, http.StatusInternalServerError, msg, "")}
+	e := wrap(err, http.StatusInternalServerError, msg, "")
+	return &e
 }
 
 // IsInternalServer reports whether err was created with InternalServerf() or
 // NewInternalServer().
 func IsInternalServer(err error) bool {
 	err = Cause(err)
-	_, ok := err.(*internalServer)
+	e, ok := err.(*Err)
+	if ok {
+		return e.Code() == http.StatusInternalServerError
+	}
 	return ok
 }
 
-// notFound represents an error when something has not been found.
-type notFound struct {
-	Err
-}
+// NotFound represents an error when something has not been found.
 
 // NotFoundf returns an error which satisfies IsNotFound().
 func NotFoundf(format string, args ...interface{}) error {
-	return &notFound{wrap(nil, http.StatusNotFound, format, "", args...)}
+	e := wrap(nil, http.StatusNotFound, format, "", args...)
+	return &e
 }
 
 // NewNotFound returns an error which wraps err that satisfies
 // IsNotFound().
 func NewNotFound(err error, msg string) error {
-	return &notFound{wrap(err, http.StatusNotFound, msg, "")}
+	e := wrap(err, http.StatusNotFound, msg, "")
+	return &e
 }
 
 // IsNotFound reports whether err was created with NotFoundf() or
 // NewNotFound().
 func IsNotFound(err error) bool {
 	err = Cause(err)
-	_, ok := err.(*notFound)
+	e, ok := err.(*Err)
+	if ok {
+		return e.Code() == http.StatusNotFound
+	}
 	return ok
 }
 
-// unauthorized represents an error when an operation is unauthorized.
-type unauthorized struct {
-	Err
-}
+// Unauthorized represents an error when an operation is unauthorized.
 
 // Unauthorizedf returns an error which satisfies IsUnauthorized().
 func Unauthorizedf(format string, args ...interface{}) error {
-	return &unauthorized{wrap(nil, http.StatusUnauthorized, format, "", args...)}
+	e := wrap(nil, http.StatusUnauthorized, format, "", args...)
+	return &e
 }
 
 // NewUnauthorized returns an error which wraps err and satisfies
 // IsUnauthorized().
 func NewUnauthorized(err error, msg string) error {
-	return &unauthorized{wrap(err, http.StatusUnauthorized, msg, "")}
+	e := wrap(err, http.StatusUnauthorized, msg, "")
+	return &e
 }
 
 // IsUnauthorized reports whether err was created with Unauthorizedf() or
 // NewUnauthorized().
 func IsUnauthorized(err error) bool {
 	err = Cause(err)
-	_, ok := err.(*unauthorized)
+	e, ok := err.(*Err)
+	if ok {
+		return e.Code() == http.StatusUnauthorized
+	}
 	return ok
 }
 
-// notImplemented represents an error when something is not
+// NotImplemented represents an error when something is not
 // implemented.
-type notImplemented struct {
-	Err
-}
 
 // NotImplementedf returns an error which satisfies IsNotImplemented().
 func NotImplementedf(format string, args ...interface{}) error {
-	return &notImplemented{wrap(nil, http.StatusNotImplemented, format, "", args...)}
+	e := wrap(nil, http.StatusNotImplemented, format, "", args...)
+	return &e
 }
 
 // NewNotImplemented returns an error which wraps err and satisfies
 // IsNotImplemented().
 func NewNotImplemented(err error, msg string) error {
-	return &notImplemented{wrap(err, http.StatusNotImplemented, msg, "")}
+	e := wrap(err, http.StatusNotImplemented, msg, "")
+	return &e
 }
 
 // IsNotImplemented reports whether err was created with
 // NotImplementedf() or NewNotImplemented().
 func IsNotImplemented(err error) bool {
 	err = Cause(err)
-	_, ok := err.(*notImplemented)
+	e, ok := err.(*Err)
+	if ok {
+		return e.Code() == http.StatusNotImplemented
+	}
 	return ok
 }
 
-// badRequest represents an error when a request has bad parameters.
-type badRequest struct {
-	Err
-}
+// BadRequest represents an error when a request has bad parameters
 
 // BadRequestf returns an error which satisfies IsBadRequest().
 func BadRequestf(format string, args ...interface{}) error {
-	return &badRequest{wrap(nil, http.StatusBadRequest, format, "", args...)}
+	e := wrap(nil, http.StatusBadRequest, format, "", args...)
+	return &e
 }
 
 // NewBadRequest returns an error which wraps err that satisfies
 // IsBadRequest().
 func NewBadRequest(err error, msg string) error {
-	return &badRequest{wrap(err, http.StatusBadRequest, msg, "")}
+	e := wrap(err, http.StatusBadRequest, msg, "")
+	return &e
 }
 
 // IsBadRequest reports whether err was created with BadRequestf() or
 // NewBadRequest().
 func IsBadRequest(err error) bool {
 	err = Cause(err)
-	_, ok := err.(*badRequest)
+	e, ok := err.(*Err)
+	if ok {
+		return e.Code() == http.StatusBadRequest
+	}
 	return ok
 }
 
-// methodNotAllowed represents an error when an HTTP request
+// MethodNotAllowed represents an error when an HTTP request
 // is made with an inappropriate method.
-type methodNotAllowed struct {
-	Err
-}
 
 // MethodNotAllowedf returns an error which satisfies IsMethodNotAllowed().
 func MethodNotAllowedf(format string, args ...interface{}) error {
-	return &methodNotAllowed{wrap(nil, http.StatusMethodNotAllowed, format, "", args...)}
+	e := wrap(nil, http.StatusMethodNotAllowed, format, "", args...)
+	return &e
 }
 
 // NewMethodNotAllowed returns an error which wraps err that satisfies
 // IsMethodNotAllowed().
 func NewMethodNotAllowed(err error, msg string) error {
-	return &methodNotAllowed{wrap(err, http.StatusMethodNotAllowed, msg, "")}
+	e := wrap(err, http.StatusMethodNotAllowed, msg, "")
+	return &e
 }
 
 // IsMethodNotAllowed reports whether err was created with MethodNotAllowedf() or
 // NewMethodNotAllowed().
 func IsMethodNotAllowed(err error) bool {
 	err = Cause(err)
-	_, ok := err.(*methodNotAllowed)
+	e, ok := err.(*Err)
+	if ok {
+		return e.Code() == http.StatusMethodNotAllowed
+	}
 	return ok
 }
